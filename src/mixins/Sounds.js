afefc11029430ce30b20e4629c5aac3152fc846a
= <=>
2019-12-08 18:31:33 +0000

    got it

--- src/mixins/Sounds.js
+++ src/mixins/Sounds.js
@@ -22,79 +22,82 @@ export default {
     data () {
       return {
         numDir: 'sound/forvo/numbers/',
-        nums:[22,3,32,12,1,1,0,65,87]
+        queue:[5,4,'How are you today mr bonobo?',2, 'who goes there mr mr mr booooy',1,0, 'It is friday night and I feel good'],
+        howling: 0,
+        s: null
       }
     },
     methods: {
-      test() {
-        console.log(this.nums)
-        if(this.nums.length) {
-          let num = this.nums.pop()
-          let i = this.playSound(this.numDir, num)//.then((token)=>{
-          console.log('beep',i)
-          
+      playAll() {
+        console.log(this.queue)
+        if(this.queue.length) {
+          let utterance = this.queue.pop()
+          let i = this.playSound(this.numDir, utterance)
           if(i instanceof Promise) {
-            console.log('baw')
-            i.then((token)=>{
-              console.log('daw', token)
-              this.test()
+            i.then(()=>{
+              this.playAll()
             })
           }
-          else if(typeof i !== "undefined"){
-            this.test()
-          }
-          else{
-            setTimeout(()=> {
-              console.log('ska')
-              this.test()
-            }, 2000)
-          }
-          
         }
       },
-      playSound(dir, s) {
-        console.log('playsound', dir, s)
+      ensureNotHowling() {
+        let _this = this
+        return new Promise(function (resolve) {
+            (function waitForFoo(){
+                if (!_this.howling) return resolve();
+                setTimeout(waitForFoo, 30);
+            })();
+        });
+      },
+      playSound(dir,s) {
+        this.s = s
+        console.log('playsound', dir, this.s)
         try {
-          let path = dir + s + '.mp3'
+          let path = dir + this.s + '.mp3'
           require('../../public/'+path)
-          this.playHowl(path, s)
+          return this.playHowl(path)
         } catch {
-          this.playTTS(s)
+          return this.playTTS()
         }    
       },
-      playHowl(path, s) {
-        try {
-          console.log('playhowl', path, s)
-          var sound = new Howl({
-            src: path,
-            onloaderror: this.playTTS
-          });
-          sound.on('end', ()=> {
-            return 1
-          })
-          sound.play();
-        }
-        catch {
-          return 0
-        }
+      playHowl(path) {
+        console.log('playhowl', path)
+        this.howling = 1
+        var sound = new Howl({
+          src: path,
+          onloaderror: () => {
+            return this.playTTS()},
+          onend: () => {
+            console.log('howler onend')
+            this.howling = 0
+          }
+        });
+        sound.play();
+        return this.ensureNotHowling(this.howling)
       },
-      playTTS(s) {
-        console.log('playTTS', s)
+      playTTS() {
+        console.log('playTTS', this.s)
         let rate = 1 + Math.random()/10 - Math.random()/10
         let pitch = 1 + Math.random()/15 - Math.random()/15
         console.log(rate, pitch)
         speech.setRate(rate)
         speech.setPitch(pitch)
         return speech.speak({
-          text: String(s),
+          text: String(this.s),
           queue: true,
-        }).then(() => {
-          console.log('tts done')
-          return 1
-        }).catch(e => {
-          console.log("An error occurred :", e)
-          return 0
+          listeners: {
+            onend: () => {
+              console.log("End utterance")
+            }
+          }
         })
+        // }).then(() => {
+        //   console.log('tts done')
+        //   return 1
+        // }).catch(e => {
+        //   console.log("An error occurred :", e)
+        //   return 0
+        // })
       }
     },
     
