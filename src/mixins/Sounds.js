044e83f81bcfbee451ddbcd17f02088acf4e7428
= <=>
2019-12-10 23:40:15 +0000

    something that works minus feedback and bugs

--- src/mixins/Sounds.js
+++ src/mixins/Sounds.js
@@ -105,6 +105,9 @@ export default {
             }
           }
         })
+      },
+      playGongSound() {
+        this.playHowl('sound/gong.mp3')
       }
     },
     
--- src/views/practice/numbers/NumberPractice.vue
+++ src/views/practice/numbers/NumberPractice.vue
@@ -15,6 +15,22 @@
           <v-slider v-model="rounds" thumb-label min="5" max="10" label="Rounds"></v-slider>
           <v-btn v-if="!queue.length"  @click="enqueueAndPlay()">Play</v-btn>
         </v-flex>
+        <v-row
+        class="mb-6"
+        >
+      <v-col>
+        <v-btn @click="submitAnswer(choiceAs)">{{choiceAs[answerIndex]}}</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn @click="submitAnswer(choiceBs)">{{choiceBs[answerIndex]}}</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn @click="submitAnswer(choiceCs)">{{choiceCs[answerIndex]}}</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn @click="submitAnswer(choiceDs)">{{choiceDs[answerIndex]}}</v-btn>
+      </v-col>
+    </v-row>
       </v-layout>
     </v-container>   
   </div>
@@ -30,32 +46,158 @@ export default {
       start: 0,
       end: 10,
       rounds: 5,
-      answers: []
+      correctAnswers: [],
+      choiceAs: [],
+      choiceBs: [],
+      choiceCs: [],
+      choiceDs: [],
+      givenAnswers: [],
+      roundIndex: 1,
     }
   },
   computed: {
     sleepTime() {
-      return 500*(~~(4/this.difficulty))
+      return 300*(~~(5/this.difficulty))
+    },
+    answerIndex() {
+      return this.rounds - this.roundIndex
     }
   },
   methods: {
+    submitAnswer(choiceList) {
+      let answer = choiceList[this.answerIndex]
+      this.givenAnswers.unshift(answer)
+      this.roundIndex += 1
+      console.log(this.givenAnswers)
+      console.log(this.correctAnswers)
+    },
+    reset(){
+      this.correctAnswers = []
+      this.choiceAs = []
+      this.choiceBs = []
+      this.choiceCs = []
+      this.choiceDs = []
+      this.givenAnswers = []
+      this.roundIndex = 1
+    },
     enqueueAndPlay() {
-      console.log(this.sleepTime)
-
-      console.log(this.start, this.end, typeof(this.end))
+      this.playGongSound()
+      setTimeout(this.reset(), 5000)
       for(let i = 0; i<this.rounds; i++) {
         let num = this.randomRange(this.start,this.end+1)
         this.queue.push(num)
-        this.answers.push(num)
+        this.addRound(num)
       }
       this.playAll(this.sleepTime)
     },
+    addRound(num) {
+      this.correctAnswers.push(num)
+      let choice = this.randomRange(0,4)
+      if(choice == 0) {
+        this.choiceAs.push(num)
+        this.makeOtherChoices(num, this.choiceBs, this.choiceCs, this.choiceDs)
+      } else if(choice == 1) {
+        this.choiceBs.push(num)
+        this.makeOtherChoices(num, this.choiceCs, this.choiceDs, this.choiceAs)
+      } else if(choice == 2) {
+        this.choiceCs.push(num)
+        this.makeOtherChoices(num, this.choiceDs, this.choiceAs, this.choiceBs)
+      } else if(choice == 3) {
+        this.choiceDs.push(num)
+        this.makeOtherChoices(num, this.choiceAs, this.choiceBs, this.choiceCs)
+      }
+    },
+    makeOtherChoices(num, x, y, z) {
+      let numerals = String(num).split('')
+      let answers = []
+      // Get legit permutations
+      let permutator = this.permute(numerals.slice())
+      let perm = permutator.next()
+      while(!perm.done) {
+        answers.push(perm.value)
+        perm = permutator.next()
+      }
+      // If not enough answers, start mutating permutations
+      let alreadyMutated = new Set()
+      answers.push(numerals)
+      while(answers.length < 4) {
+        let i = this.randomRange(0, answers.length)
+        if(!alreadyMutated.has(i)){
+          alreadyMutated.add(i)
+          if(alreadyMutated.length > 2 || alreadyMutated.length >= answers.length) {
+            alreadyMutated = new Set()
+          }
+          answers.push(this.mutatePerm(answers[i]))
+        }
+      }
+      // Get rid of actual answer from answer list
+      let answeri = answers.indexOf(numerals);
+      answers.splice(answeri, 1);
+      // Add answers to choices lists
+      x.push(this.formatFakeAnswer(answers))
+      y.push(this.formatFakeAnswer(answers))
+      z.push(this.formatFakeAnswer(answers))
+    },
+    formatFakeAnswer(a) {
+      let s = a.splice(this.randomRange[0,a.length],1)[0].join('')
+      if(s.length==1) {
+        return s
+      }
+      let z = s[0]
+      while(z == '0') {
+        s = s.slice(1)
+        if(s.length==1) {
+          return s
+        }
+        z = s[0]
+      }
+      return s
+    },
+    mutatePerm(perm) {
+      let i = this.randomRange(0, perm.length)
+      let n = perm[i]
+      let m = null
+      if(n < 2 || (n < 7 && Math.random()>0.5)) {
+        m = this.randomRange(n,9)
+      } else {
+        m = this.randomRange(0,n)
+      }
+      let mutated = perm.slice()
+      mutated[i] = m
+      return mutated
+    },
     randomRange(min, max) {
     let num = Math.floor(Math.random() * (max - min) + min);
-    console.log(min, max, num)
     return num
     },
-    
+    *permute(permutation) {
+      var length = permutation.length,
+      c = Array(length).fill(0),
+      i = 1, k, p;
+      let prev = new Set()
+      prev.add(permutation.join(''))
+
+      while (i < length) {
+      if (c[i] < i) {
+        k = i % 2 && c[i];
+        p = permutation[i];
+        permutation[i] = permutation[k];
+        permutation[k] = p;
+        ++c[i];
+        i = 1;
+        let n = permutation.slice();
+        let s = n.join('')
+        if(!prev.has(s)) {
+          prev.add(s)
+          yield n
+        } 
+        
+      } else {
+        c[i] = 0;
+        ++i;
+      }
+  }
+}
   },
   mixins: [Sounds],
 }
