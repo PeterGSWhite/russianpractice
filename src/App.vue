75d86b5293e28798bcbe3e469645222842b3b51d
= <=>
2019-12-26 13:25:44 +0000

    footer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
   <v-app>
     <v-content>
       <router-view></router-view>
+      <v-footer
+      absolute
+      class="font-weight-medium"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Made by Peter White - petergswhite@gmail.com</strong>
+      </v-col>
+    </v-footer>
     </v-content>
   </v-app>
 </template>
